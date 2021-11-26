fb0ce5a391fb330811dc5caa8e400eec8c741adf
maxmadden-me <76030585+maxmadden-me@users.noreply.github.com>
2021-11-26 11:20:21 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,10 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader("Choose a file")    
-dataframe = pd.read_csv(uploaded_file)
-st.write(dataframe)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
      
 st.write("""
 #My First App
