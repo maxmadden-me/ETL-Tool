8493166a1c5efd26fed30d7185b0628bf1935797
maxmadden-me <76030585+maxmadden-me@users.noreply.github.com>
2021-11-26 11:12:36 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,13 @@
 import streamlit as st
 import pandas as pd
 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
+        
+
 st.write("""
 #My First App
 Hello *World!*
